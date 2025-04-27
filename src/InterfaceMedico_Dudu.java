9645bf38493f9e9c1f53dab746786761a5f4b7ea
DuduBertol <dudubertoldev@gmail.com>
2025-04-27 20:22:10 -0300

    Added files for eachone

---
+++ src/InterfaceMedico_Dudu.java
@@ -0,0 +1,12 @@
+public class InterfaceMedico_Dudu {
+
+    //A. Interface do Médico permite realizar as seguintes operações de pesquisa:
+    //1. Quais são todos os pacientes de um determinado médico?
+    //2. Quais são todas as consultas agendadas para um determinado médico
+    //em determinado período (definido por uma data inicial e uma data final),
+    //na ordem crescente dos horários? (O período pode cobrir tanto o passado
+    //como o futuro.)
+    //3. Quais são os pacientes de um determinado médico que não o consulta há
+    //mais que um determinado tempo (em meses)?
+
+}
---
+++ src/InterfacePaciente_Rafa.java
@@ -0,0 +1,12 @@
+public class InterfacePaciente_Rafa {
+
+    //B. Interface do Paciente permite realizar as seguintes operações de pesquisa:
+    //1. Quais são todos os médicos de um paciente, isto é, médicos que o
+    //paciente já se consultou ou tem consulta agendada?
+    //2. Quando ocorreram todas as consultas de um determinado paciente com
+    //determinado médico? (Somente consultas realizadas no passado são
+    //consideradas.)
+    //3. Quais são todas as consultas agendadas que um determinado paciente
+    //possui? (Somente consultas agendadas para o futuro são consideradas.)
+
+}
