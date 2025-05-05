9517ab56e87d24738fc589eec38bc7e065af5b92
kaiube <rafaelmartins95327@gmail.com>
2025-05-05 19:47:19 -0300

    interface Paciente getIDfromCPF(tentativa)

--- src/InterfacePaciente_Rafa.java
+++ src/InterfacePaciente_Rafa.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class InterfacePaciente_Rafa {
 
     //B. Interface do Paciente permite realizar as seguintes operações de pesquisa:
@@ -9,4 +12,45 @@ public class InterfacePaciente_Rafa {
     //3. Quais são todas as consultas agendadas que um determinado paciente
     //possui? (Somente consultas agendadas para o futuro são consideradas.)
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        PacientesDB pacientesDB = new PacientesDB();
+        pacientesDB.readCSV();
+
+        ConsultasDB consultasDB = new ConsultasDB();
+        consultasDB.readCSV();
+// public ArrayList<String> getIDFromCpfs(String cpf){
+//        ArrayList<String> id = new ArrayList<>();
+//
+//        for(int i = 0; i < this.consultasTable.size(); i++) {
+//            List<String> linha = this.consultasTable.get(i);
+//
+//            String actualCpf = (linha.get(2));
+//            if(actualCpf == cpf) {
+//                id.add(linha.get(3));
+//            }
+//        }
+//
+//        return id;
+//    }
+
+
+        //  Um Sonho(Pegar Id de medico pelo Cpf)
+        //ArrayList<String> IdMedicos = consultasDB.getIDFromCpfs(cpf);
+
+
+        //Recebe CPF do Queride
+        System.out.println("Digite seu CPF: ");
+        String cpf = input.nextLine();
+
+        //pega as consultas do CPF
+        ArrayList<Consulta> consultasDoPaciente = consultasDB.getConsultasByCPF(cpf);
+
+
+
+
+        System.out.println(pacientesDB.getNameFromCpf(cpf));
+
+    }
 }
